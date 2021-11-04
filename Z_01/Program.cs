8f22cd7ac806a37e190446b191c62e6a62e84260
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-04 20:33:18 +0300

    первая фиксация

---
+++ Z_01/Program.cs
@@ -0,0 +1,7 @@
+﻿// Семинар СШ 03 (04-11-21) Задача 1
+int a = 6;
+Console.WriteLine("Веедите число");
+string s = Console.ReadLine();
+a = Convert.ToInt32(s);
+Console.WriteLine("Квадрат числа = ");
+Console.WriteLine(Math.Pow(a,2));
