d0b198a6cba77171e80fe5420eac93c47f8d4712
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-04 21:41:03 +0300

    вторая  фиксация

---
+++ Z_02/Program.cs
@@ -0,0 +1,37 @@
+﻿// 1. По двум заданным числам проверять является ли первое квадратом второго
+
+// Семинар СШ 03 (04-11-21) Задача 1
+//int a = 6;
+////Console.WriteLine("Веедите число");
+//string s = Console.ReadLine();
+//a = Convert.ToInt32(s);
+//Console.WriteLine("Квадрат числа = ");
+//Console.WriteLine(Math.Pow(a,2));
+
+int a = 4;
+int b = 17;
+
+Console.WriteLine("Веедите первое число");
+string a1 = Console.ReadLine();
+a = Convert.ToInt32(a1);
+
+Console.WriteLine("Веедите второе число");
+string b1 = Console.ReadLine();
+b = Convert.ToInt32(b1);
+
+double ak = Math.Pow(a,2);
+//Console.WriteLine(ak);
+
+if (ak == b)
+{
+    Console.WriteLine("первое квадрат второго");
+} 
+else
+{
+    Console.WriteLine("первое не квадрат второго");  
+}
+Console.WriteLine("Программа завершина");
+
+  
+
+
---
+++ Z_03/Program.cs
@@ -0,0 +1,59 @@
+﻿// 3. По заданному номеру дня недели вывести его название
+// Семинар СШ 03 (04-11-21) Задача 1
+//int a = 6;
+//Console.WriteLine("Веедите число");
+//string s = Console.ReadLine();
+//a = Convert.ToInt32(s);
+//Console.WriteLine("Квадрат числа = ");
+//Console.WriteLine(Math.Pow(a,2));
+
+
+string A1 = "пон";
+string A2 = "вт";
+string A3 = "ср";
+string A4 = "чет";
+string A5 = "пят";
+string A6 = "суб";
+string A7 = "вос";
+
+int a;
+
+Console.WriteLine("Веедите число от 1 до 7");
+string n = Console.ReadLine();
+a = Convert.ToInt32(n);
+
+if (a == 1) 
+{
+    Console.WriteLine("День недели");
+    Console.WriteLine(A1);
+}
+if (a == 2)
+{
+    Console.WriteLine("День недели");
+    Console.WriteLine(A2);    
+}
+if (a == 3)
+{
+    Console.WriteLine("День недели");
+    Console.WriteLine(A3);    
+}
+if (a == 4)
+{
+    Console.WriteLine("День недели");
+    Console.WriteLine(A4);    
+}
+if (a == 5)
+{
+    Console.WriteLine("День недели");
+    Console.WriteLine(A5);    
+}
+if (a == 6)
+{
+    Console.WriteLine("День недели");
+    Console.WriteLine(A6);    
+}
+if (a == 7)
+{
+    Console.WriteLine("День недели");
+    Console.WriteLine(A7);    
+}
