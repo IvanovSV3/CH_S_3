270f39e9b6ea24dc5d9f1edb4753de558f94dd96
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-08 21:19:46 +0300

    Задача 20  фиксация

---
+++ Z_20/Program.cs
@@ -0,0 +1,10 @@
+﻿// 20. Ввести номер четверти, показать диапазоны для возможных координат
+Console.WriteLine("Веедите номер четверти №1; №2; №3; №4");
+string s = Console.ReadLine();
+ double x;
+x = Convert.ToInt32(s);
+
+if (x == 2) Console.WriteLine("Значение координат x < 0 & Y > 0");
+if (x == 1) Console.WriteLine("Значение координат x > 0 & Y > 0");
+if (x == 3) Console.WriteLine("Значение координат x < 0 & Y < 0");
+if (x == 4) Console.WriteLine("Значение координат x > 0 & Y < 0");
