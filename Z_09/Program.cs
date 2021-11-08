6d01503d2b605f6e674ec4e22d0e6387c42a1ede
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-08 20:38:18 +0300

    Задача 09  фиксация

---
+++ Z_09/Program.cs
@@ -0,0 +1,11 @@
+﻿// 9. Показать последнюю цифру трёхзначного числа
+
+int a;
+Console.WriteLine("Ведите трех значное число");
+string s = Console.ReadLine();
+a = Convert.ToInt32(s);
+
+
+Console.WriteLine("Последняя цифра ");
+Console.WriteLine(a%10);
+
