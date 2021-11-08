194868987423f555327a8b540ded2141a60ac6a7
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-08 18:38:26 +0300

    Задача 15  фиксация

---
+++ Z_04/Program.cs
@@ -0,0 +1,11 @@
+﻿// 35. Определить, присутствует ли в заданном массиве, некоторое число
+
+// Семинар СШ 03 (04-11-21) Задача 1
+int a = 6;
+Console.WriteLine("Веедите число");
+string s = Console.ReadLine();
+a = Convert.ToInt32(s);
+Console.WriteLine("Квадрат числа = ");
+Console.WriteLine(Math.Pow(a,2));
+
+int[] serg{1, 2, 4, 5, 6, 7, 10};
---
+++ Z_15/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 15 Дано число. Проверить кратно ли оно 7 и 23
+Console.WriteLine("Ведите целое число ");
+string s = Console.ReadLine();
+double a = Convert.ToInt32(s);
+//int f = Int32.Parse(s);
+//Console.WriteLine(f);
+// double d = Math.Round(a);
+// Console.WriteLine(d);
+
+double d = a % 7;
+double d1 = a % 23;
+
+if (d == 0)
+{
+    Console.WriteLine("Число кратно 7");
+}
+else
+{
+    if (d1 == 0)
+{
+  Console.WriteLine("Число кратно 23");  
+}
+
+}
+
+if (d > 0 && d1 > 0)
+{
+    Console.WriteLine("Число не кратно 23 и 7"); 
+}
+
+
+
+
+
+
+
+// Семинар СШ 03 (04-11-21) Задача 1
+// int a = 6;
+// Console.WriteLine("Веедите число");
+// string s = Console.ReadLine();
+// a = Convert.ToInt32(s);
+// Console.WriteLine("Квадрат числа = ");
+// Console.WriteLine(Math.Pow(a,2));
