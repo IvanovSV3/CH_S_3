5b841710e86acbdf0b7883d72a69cdb24cfb3c39
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-09 12:29:55 +0300

    Задача 25  фиксация

---
+++ Z_25/Program.cs
@@ -0,0 +1,16 @@
+﻿// 25. Найти сумму чисел от 1 до А
+Console.WriteLine("Ведите целое число А");
+string a = Console.ReadLine();
+int A;
+int Summa = 0;
+int index = 1;
+A = Convert.ToInt32(a);
+// Console.WriteLine(A);
+
+while (index <= A)
+{
+     Summa = Summa + index;
+     index = index + 1;
+}
+Console.WriteLine("Сумма чисел:");
+Console.WriteLine(Summa);
