1ab718ecb0b05c89d4163c5a0b96e06f3b6ff714
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-09 12:02:06 +0300

    Задача 24  фиксация

---
+++ Z_24/Program.cs
@@ -0,0 +1,19 @@
+﻿//24. Найти кубы чисел от 1 до N
+Console.WriteLine("Введите чило элементов N");
+string n = Console.ReadLine();
+int N = Convert.ToInt32(n);
+// Console.WriteLine(N);
+
+int[]  tabl= new int[N];
+ int index = 0;
+double index_1 = 1;
+
+while (index < N)
+{    
+    // tabl[index] = index + 2;
+     tabl[index] = Convert.ToInt32(Math.Pow(index_1,3));
+     Console.WriteLine(tabl[index]);
+     index = index + 1;
+     index_1++;
+
+}
