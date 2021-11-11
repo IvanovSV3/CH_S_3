f27aa3ba36088dc855c42a15a508b3b4634d18cd
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-11 20:02:41 +0300

    Задача 35  фиксация

---
+++ Z_35/Program.cs
@@ -0,0 +1,19 @@
+﻿// 35. Определить, присутствует ли в заданном массиве, некоторое число 
+int[] ser = new int[20];
+Console.WriteLine("Ведите чило челое ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < ser.Length; i++)
+{
+    ser[i] = new Random().Next(1, 20);
+    Console.Write(ser[i] + "");
+} 
+
+for (int i = 0; i < ser.Length; i++)
+{
+    if (ser[i] == a)
+    {
+        Console.WriteLine("Найдено совпадение ");
+        Console.WriteLine(ser[i]);
+    }
+}
