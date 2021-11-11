7f064513c8e844c07ccffa6fa8c81520577d5bd6
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-11 19:05:53 +0300

    Задача 07  фиксация

---
+++ Z_08/Program.cs
@@ -0,0 +1,20 @@
+﻿//9. Показать четные числа от 1 до N
+
+int a, index;
+Console.WriteLine("Веедите число");
+string s = Console.ReadLine();
+a = Convert.ToInt32(s);
+double b = a % 2;
+index = 1;
+ 
+
+while (index <= a)
+{
+    b = index%2;
+    
+    if (b == 0)
+    {
+        Console.WriteLine(index);
+    }
+    index = index + 1;
+}
