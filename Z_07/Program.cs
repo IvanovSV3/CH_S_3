8965523285ef1094e14b7656095be7ed19a1ea6d
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-11 18:48:45 +0300

    Задача 07  фиксация

---
+++ Z_07/Program.cs
@@ -0,0 +1,16 @@
+﻿// 7. Показать числа от -N до N
+
+int a;
+int b;
+int index = 0;
+Console.WriteLine("Веедите целое число");
+string s = Console.ReadLine();
+a = Convert.ToInt32(s);
+b = a * -1;
+
+while (b <= a)
+{
+   Console.WriteLine(b);  
+   b = b + 1;
+   // index = index +1;
+}
