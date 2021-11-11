9fb9dadbb0be5a9e23561d20f627af275962170d
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-11 18:23:27 +0300

    Задача 06  фиксация

---
+++ Z_06/Program.cs
@@ -0,0 +1,14 @@
+﻿// 6. Выяснить является ли число чётным
+int a = 6;
+Console.WriteLine("Веедите число");
+string s = Console.ReadLine();
+a = Convert.ToInt32(s);
+double b = a % 2;
+if (b == 0)
+{
+   Console.WriteLine("число четное"); 
+}
+else
+{
+    Console.WriteLine("число не четное"); 
+}
