b05c897fca0640b3417b0cc0a65013e0badbeb1e
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-11 19:36:27 +0300

    Задача 27  фиксация

---
+++ Z_27/Program.cs
@@ -0,0 +1,14 @@
+﻿// 27. Определить количество цифр в числе
+
+Console.WriteLine("Веедите целое число");
+// string s = Console.ReadLine();
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 0;
+
+while ( a != 0)
+{
+     a = a /10;
+     b ++;
+}
+Console.WriteLine("количество чисел");
+Console.WriteLine(b);
