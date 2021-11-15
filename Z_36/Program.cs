945fb84eae1ac0d5b125f8c756095380e0d896e1
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-15 11:08:20 +0300

    Задача 36  фиксация

---
+++ Z_36/Program.cs
@@ -0,0 +1,22 @@
+﻿// 36. Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+int[] serg = new int[10];
+int k1 = 0; 
+int k2 = 0;
+double k;
+
+for (int i = 0; i < serg.Length; i++)
+{
+    serg[i]= new Random().Next(100,999);
+    Console.Write(serg[i] + " ");
+    k = serg[i]%2;
+    if (k == 0)
+    {
+        k1 = k1+1;
+    }
+    else
+    {
+        k2 = k2+1;
+    }
+}
+Console.WriteLine("Количество четных " + k1);
+Console.WriteLine("Количество не четных " + k2);
