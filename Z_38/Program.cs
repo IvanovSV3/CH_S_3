643bc0fc71be0379b549c77fa5461910f3fa5aa3
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-15 12:03:35 +0300

    Задача 38  фиксация

---
+++ Z_38/Program.cs
@@ -0,0 +1,19 @@
+﻿// 38. Найти сумму чисел одномерного массива стоящих на нечетной позиции
+int H=4;
+int[] ser = new int[H];
+double k;
+int Sum=0;
+
+
+for (int i = 0; i < ser.Length; i++)
+{
+    ser[i] = new Random().Next(0,100);
+    Console.Write(ser[i] + " ");
+    
+    k = i%2;
+    if (k == 0)
+    {
+       Sum = Sum + ser[i];
+    }
+}
+Console.WriteLine("Сумма = " +Sum);
