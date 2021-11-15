2f9792b581de9e63e3d8c5b2a0e77cf29d67631f
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-15 13:59:42 +0300

    Задача 39  фиксация

---
+++ Z_39/Program.cs
@@ -0,0 +1,26 @@
+﻿// 39. Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+int N;
+Console.Write("Ведите размер массива  ");
+N = Convert.ToInt32(Console.ReadLine());
+int[] ser = new int[N];
+
+int sum1=0;
+int sum2=N-1;
+int P;
+
+for (int i = 0; i < ser.Length; i++)
+{
+    ser[i] = new Random().Next(1,100);
+    Console.Write(ser[i] + " ");
+    
+}
+for (int i = 0; i < ser.Length; i++)
+{
+   if (sum2 > sum1)
+    {
+        P = ser[sum1] * ser[sum2];
+        Console.WriteLine("Произведение " + P);
+    }
+    sum1=sum1+1;
+    sum2 = sum2 -1; 
+}
