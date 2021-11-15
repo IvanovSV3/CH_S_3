32a2893547d107e6e7979c9f35f90cbe683f0337
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-15 11:38:36 +0300

    Задача 31  фиксация

---
+++ Z_31/Program.cs
@@ -0,0 +1,10 @@
+﻿// 31. Задать массив из 8 элементов и вывести их на экран 
+
+int[] ser = new int[8];
+
+
+for (int i = 0; i < ser.Length; i++)
+{
+    ser[i] = new Random().Next(1, 20);
+    Console.Write(ser[i] + " ");
+}
