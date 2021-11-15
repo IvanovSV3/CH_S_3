773dc06b6938e3408a930457ec184adecf769127
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-15 11:33:29 +0300

    Задача 32  фиксация

---
+++ Z_32/Program.cs
@@ -0,0 +1,8 @@
+﻿//32. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран 
+int[] ser=new int [8];
+for (int i = 0; i < ser.Length; i++)
+{
+    ser[i]=new Random().Next(0,2);
+    Console.WriteLine(ser[i]);
+
+}
