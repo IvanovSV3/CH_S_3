7b81c19808769a1e570948e5e3cf8a3b86bc7013
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-15 10:45:56 +0300

    Задача 34  фиксация

---
+++ Z_34/Program.cs
@@ -0,0 +1,21 @@
+﻿// 34. Написать программу замену элементов массива на противоположные
+int[] ser = new int[10];
+// Console.WriteLine("Ведите чило челое ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < ser.Length; i++)
+{
+    ser[i] = new Random().Next(-10, 10);
+    Console.Write(ser[i] + " ");
+    // ser[i] = ser[i] * -1;
+    // Console.Write(ser[i] + " ");
+}
+Console.Write("Новый массив   ");
+
+for (int i = 0; i < ser.Length; i++)
+{
+    // ser[i] = new Random().Next(-10, 10);
+    // Console.Write(ser[i] + " ");
+    ser[i] = ser[i] * -1;
+    Console.Write(ser[i] + " ");
+}
