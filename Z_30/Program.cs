9e40324ea9295ab0705fee1ab9255f4c93f0c64f
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-16 21:07:28 +0300

    Задача 30  фиксация

---
+++ Z_30/Program.cs
@@ -0,0 +1,19 @@
+﻿// 30. Показать кубы чисел, заканчивающихся на четную цифру
+Random temp = new Random();
+int nuber = temp.Next(1,20);
+Console.WriteLine(nuber);
+double cube;
+double cube2;
+
+for (int i = 1; i <= nuber; i++)
+{
+   cube = Math.Pow(i, 3);
+   Console.WriteLine(cube);
+   cube2 = cube % 10 % 2;
+   if (cube2 == 0)
+   {
+       Console.WriteLine("Куб с четным концом = " +cube);
+   }
+}
+
+
