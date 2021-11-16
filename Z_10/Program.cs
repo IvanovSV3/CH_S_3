db31f4266c2a1c56a3e688089e389f4bb3673bfa
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-16 14:05:44 +0300

    Задача 10  фиксация

---
+++ Z_10/Program.cs
@@ -0,0 +1,11 @@
+﻿// 10. Показать вторую цифру трёхзначного числа
+Random  ran = new Random();
+int number;
+number = ran.Next(100,1000);
+Console.WriteLine(number);
+
+string str = Convert.ToString(number);
+
+char[] mas = str.ToCharArray();
+
+Console.WriteLine("вторая цифра " +mas[1]);
