8c4633e28fced891b4841584ebf517c6160d5dbf
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-16 22:21:05 +0300

    Задача 18  фиксация

---
+++ Z_18/Program.cs
@@ -0,0 +1,12 @@
+﻿// 18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+bool X = false;
+bool X1 = true;
+
+bool Y = false;
+bool Y1 = true;
+
+bool f1 = !(X || Y);
+bool f2 = !X && !Y;
+
+Console.WriteLine(f1);
+Console.WriteLine(f2);
