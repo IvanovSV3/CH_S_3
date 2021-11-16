f6216d6521a1f51c1a04c54c6bdb9f49efc80d83
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-16 19:01:32 +0300

    Задача 14  фиксация

---
+++ Z_14/Program.cs
@@ -0,0 +1,14 @@
+﻿// 14. Найти третью цифру числа или сообщить, что её нет
+Random temp = new Random();
+int number = temp.Next(1,1000);
+Console.WriteLine(number);
+
+if (number / 100 < 1)
+{
+   Console.Write("Третьей цифры нет");
+}
+else
+{
+   number = number % 10;
+   Console.Write("Третья цифра = " +number);
+}
