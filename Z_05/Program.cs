1265741739b2d91f93e644a903a35b30742d2abc
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-16 13:02:14 +0300

    Задача 05  фиксация

---
+++ Z_05/Program.cs
@@ -0,0 +1,12 @@
+﻿// 5. Написать программу вычисления значения функции y=f(a)
+Console.WriteLine("Введите целое число ");
+int K1 = Convert.ToInt32(Console.ReadLine());
+
+int ser(int a)
+{
+    int y;
+    y = a + 2*a;
+    return y;
+}
+
+Console.WriteLine("значение фукции " +ser(K1));
