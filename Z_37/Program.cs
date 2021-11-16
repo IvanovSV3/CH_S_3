d132c8e518fd6343360d1a2d5006e07382e6328d
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-16 21:54:59 +0300

    Задача 37  фиксация

---
+++ Z_37/Program.cs
@@ -0,0 +1,17 @@
+﻿// 37. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+Random temp = new Random();
+int[] masiv = new int[123];
+
+for (int i = 0; i < masiv.Length; i++)
+{
+    masiv[i] = temp.Next(1,500);
+    // Console.Write(masiv[i] +"  ");
+
+     if (masiv[i] < 99)
+     {
+        if (masiv[i] > 10)
+         {
+            Console.WriteLine("Пренадлежит заданному отрзку " +masiv[i]+ "  "); 
+        }
+    }
+}
