1daa404941b559688dbcc147162630bf15c7aa5b
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-16 20:45:19 +0300

    Задача 29  фиксация

---
+++ Z_29/Program.cs
@@ -0,0 +1,12 @@
+﻿// 29. Написать программу вычисления произведения чисел от 1 до N
+Random temp = new Random();
+int N = temp.Next(2,13);
+Console.WriteLine("Число N = " +N);
+int multiN = 1;
+
+
+for (int i = 2; i <= N; i++)
+{
+   multiN = multiN * i;
+   Console.WriteLine("Произведение чисел от 1 до N = " +multiN);
+}
