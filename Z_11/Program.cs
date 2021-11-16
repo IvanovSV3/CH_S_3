6c1b6c5802e0ed2cb49b91ec7dc33f59e14d3178
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-16 17:56:04 +0300

    Задача 11  фиксация

---
+++ Z_11/Program.cs
@@ -0,0 +1,20 @@
+﻿// 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+Random ser = new Random();
+int digit = ser.Next(10,100);
+Console.WriteLine(digit);
+
+int number1, number2;
+
+number1 = digit % 10;
+Console.WriteLine("Число 1 " +number1);
+number2 = digit /10 %10;
+Console.WriteLine("Число 2 " +number2);
+
+if (number1 > number2)
+{
+    Console.WriteLine("Наибольшая цифра " +number1);
+}
+else
+{
+    Console.WriteLine("Наибольшая цифра " +number2);
+}
