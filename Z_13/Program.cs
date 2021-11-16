5ea8629c156f36e095ddc3e22e39fc6ba9737ff7
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-16 18:47:43 +0300

    Задача 13  фиксация

---
+++ Z_13/Program.cs
@@ -0,0 +1,15 @@
+﻿// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+int disit = 4;
+Random numb = new Random();
+int number = numb.Next(10, 100);
+Console.WriteLine(number);
+
+int residue = number % disit;
+if (residue == 0)
+{
+    Console.WriteLine("Число кратно заданому");
+}
+else
+{
+    Console.WriteLine("Число не кратно заданому, остаток = " + residue);
+}
