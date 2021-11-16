4c2da23decb692fd1f13723224af1e8e4f37d8b4
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-16 18:31:25 +0300

    Задача 12  фиксация

---
+++ Z_12/Program.cs
@@ -0,0 +1,21 @@
+﻿// 12. Удалить вторую цифру трёхзначного числа
+Random ser = new Random();
+int number = ser.Next(100,1000);
+Console.WriteLine(number);
+
+int numb1 = number /10/10%10;
+int numb2 = number /10%10;
+int numb3 = number %10;
+Console.WriteLine(numb1);
+
+number = numb1 * 10 + numb3;
+
+Console.WriteLine("без второй цифры " +number);
+
+// string numberstrit = Convert.ToString(number);
+// char[] numbermasiv = numberstrit.ToCharArray();
+// Console.WriteLine(numbermasiv[0]);
+// Console.WriteLine(numbermasiv[1]);
+// Console.WriteLine(numbermasiv[2]);
+
+// int[] numbernew = new[2];
