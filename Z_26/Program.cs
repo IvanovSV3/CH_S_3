68318a615906465c1edf63c5e468da59df6721f2
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-16 20:28:44 +0300

    Задача 26  фиксация

---
+++ Z_26/Program.cs
@@ -0,0 +1,15 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+Random temp = new Random();
+int A = temp.Next(1,5);
+int B = temp.Next(1,6);
+Console.WriteLine("Число A = " +A);
+Console.WriteLine("Число B = " +B);
+
+
+int A1 = A;
+
+for (int i = 2; i <= B; i++)
+{
+    A1 = A1 *A;
+    Console.WriteLine(A1 +"  ");
+}
