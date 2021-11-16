7c2b951fb26fb08831735df84f210c767450ddc0
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-16 20:36:38 +0300

    Задача 28  фиксация

---
+++ Z_28/Program.cs
@@ -0,0 +1,10 @@
+﻿// 28. Подсчитать сумму цифр в числе
+Random temp = new Random();
+int number = temp.Next(100,1000);
+Console.WriteLine(number);
+int num1 = number %10; Console.WriteLine(num1);
+int num2 = number /10%10; Console.WriteLine(num2);
+int num3 = number /10/10%10; Console.WriteLine(num3);
+
+int sum = num1 + num2 + num3;
+Console.WriteLine("Сумма цифр в числе = " +sum);
