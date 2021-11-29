325699781bd08e119800bcd2ed9368d10fcc02f8
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-29 21:00:49 +0300

    Задача 48  фиксация

---
+++ Z_48/Program.cs
@@ -0,0 +1,14 @@
+﻿// 48 Показать двумерный массив размером m×n заполненный целыми числами
+
+int n = 4, m = 6;
+int[,] АrrayDu = new int[n,m];
+Random chance = new Random();
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        АrrayDu[i,j] = chance.Next(0,101);
+        Console.Write(АrrayDu[i,j]+" ");
+    }
+Console.WriteLine();
+}
