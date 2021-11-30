54ed377a9b1bf1de579369d180d4e87c7df7c919
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-11-30 18:24:58 +0300

    Задача 51  фиксация

---
+++ Z_51/Program.cs
@@ -0,0 +1,33 @@
+﻿// 51 Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+int[,] ArrayAmn = new int[4,7];
+
+int[,] ArrayCreate(int[,] arraytemp)
+{
+// Random chance = new Random();
+
+for (int i = 0; i < arraytemp.GetLength(0); i++)
+{
+    for (int j = 0; j < arraytemp.GetLength(1); j++)
+    {
+        arraytemp[i,j] = i + j;
+    }
+}
+return arraytemp;
+}
+
+void ArrayPrint(int[,] arraytemp1)
+{
+for (int i = 0; i < arraytemp1.GetLength(0); i++)
+{
+    for (int j = 0; j < arraytemp1.GetLength(1); j++)
+    {
+        Console.Write(arraytemp1[i,j]+" ");
+    }
+    Console.WriteLine();
+    }
+}
+
+ArrayAmn = ArrayCreate(ArrayAmn);
+
+ArrayPrint(ArrayAmn);
