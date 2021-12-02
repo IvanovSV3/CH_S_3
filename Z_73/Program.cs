ccda7875cfad73a131955f7ed4394f2ae67d3eab
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-12-02 21:18:44 +0300

    Задача 73  фиксация

---
+++ Z_73/Program.cs
@@ -0,0 +1,22 @@
+﻿// 73. Написать программу показывающие первые N чисел, для которых каждое следующее равно 
+// сумме двух предыдущих. Первые два элемента последовательности задаются пользователем/
+int N = 10;
+Console.WriteLine("Ведите перый элемент");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите второй элемент");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+void ShowNummber(int a, int b, int n)
+{
+int c;
+if (n > 0)
+{
+    c =  a + b;
+    a = b;
+    b = c;
+    Console.WriteLine(c);
+    n = n - 1;
+    ShowNummber(a, b, n);
+}
+}
+ShowNummber(n1, n2, N);
