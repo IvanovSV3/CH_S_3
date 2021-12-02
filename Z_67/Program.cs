2b766fb7779713aa277f07a29ec7cd6fc8fed08d
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-12-02 19:18:01 +0300

    Задача 67  фиксация

--- Z_67/Program.cs
+++ Z_67/Program.cs
@@ -1,1 +1,15 @@
 ﻿// 67 Показать натуральные числа от N до 1, N задано
+Random ser = new Random();
+int N = ser.Next(0,15);
+Console.WriteLine("N = "+N);
+
+void PrintInt(int n)
+{
+    if (n >= 0)
+    {
+       Console.WriteLine("Натуральное число = "+n);
+       n = n - 1;
+       PrintInt(n);
+    }
+}
+PrintInt(N);
