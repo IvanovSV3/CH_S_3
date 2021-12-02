d47a063847e68f4f8cc1fee9e8d27889903f510c
Ivanov_SV <Ivanov_SV3@yndex.ru>
2021-12-02 18:45:06 +0300

    Задача 66  фиксация

---
+++ Z_66/Program.cs
@@ -0,0 +1,16 @@
+﻿// 66 Показать натуральные числа от 1 до N, N задано
+Random ser = new Random();
+int N = ser.Next(0,15);
+Console.WriteLine("N = "+N);
+
+void PrintInt(int n)
+{
+    if (n >= 0)
+    {
+       Console.WriteLine("Натуральное число = "+n);
+       n = n - 1;
+       PrintInt(n);
+    }
+}
+
+PrintInt(N);
